18007bc2f1e87084daf9817e0201459a3cb2f686
SalmanSaeed24a <95068089+SalmanSaeed24a@users.noreply.github.com>
2021-12-01 11:49:28 +0500

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main () 
+{
+char c;
+printf("enter the alphbate :");
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+printf("this is vowel character\n");
+else
+printf("this is consonant character\n");
+} 
+
+
+
+
+
+
+
+
+
+
